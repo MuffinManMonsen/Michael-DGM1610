ce5167276067daedd7676eea2fe0c6ee96a7d88c
Michael <starwalker_m@outlook.com>
2019-02-11 17:19:16 -0700

    Created Animals and Collectible Coin

---
+++ 2D Game/Assets/Scripts/Assignments/Munchkin_Cat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Munchkin_Cat : Feline {
+
+
+public bool banded;
+
+	// Use this for initialization
+	void Start () {
+		print 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 2D Game/Assets/Scripts/Assignments/coinPickup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinPickup : GenericPickup {
+
+	public GameObject PC;
+
+
+	void OnTriggerEnter(Collider2D other) {
+		if(other.name == "PC")	
+			//player collects coin
+	}
+	else{
+		//not player
+	} 
+}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

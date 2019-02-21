8527ff09bac4d227b60a24dd4232182f9eb12943
Michael <starwalker_m@outlook.com>
2019-02-20 17:11:13 -0700

    Added GetKeyDown and created Code for a coin pickup.

--- 2D Game/Assets/Scripts/PlayerController.cs
+++ 2D Game/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	// Player movement variables
 	public float moveSpeed; 
 	public float jumpHeight;
+	
+	//Player grounded variables
+	public bool grounded;
 
 
 	// Use this for initialization
@@ -21,7 +25,9 @@ public class PlayerController : MonoBehaviour {
 		else if(Input.GetKey(KeyCode.A)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}	
-		else if(Input.GetKey(KeyCode.W)){
+		
+		// Makes player jump
+		if(Input.GetKeyDown(KeyCode.W)&& grounded){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,jumpHeight);
 		}
 	}

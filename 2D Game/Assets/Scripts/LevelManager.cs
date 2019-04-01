3d2b415ef770dc92136146990b1fcdee17095392
Michael Monsen <starwalker_m@outlook.com>
2019-04-01 17:16:53 -0600

    Created Respawn script

--- 2D Game/Assets/Scripts/LevelManager.cs
+++ 2D Game/Assets/Scripts/LevelManager.cs
@@ -31,9 +31,43 @@ public class LevelManager : MonoBehaviour {
 		player = GameObject.Find("Player");
 		
 	}
+
+	public void RespawnPlayer(){
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		//Generate Death Particle
+		Instantiate (deathParticle, pcRigid.transform.position, pcRigid.transform.rotation);
+		//Hide PC
+		player.SetActive(false);
+		pcRigid.GetComponent<Renderer> ().enabled = false;
+		//Gravity Reset
+		gravityStore = pcRigid.GetComponent<Rigidbody2D>().gravityScale;
+		pcRigid.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		pcRigid.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		//Point Penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		//Debug Message
+		Debug.Log ("PC Respawn");
+		//Respawn Delay
+		yield return new WaitForSeconds (RespawnDelay);
+		//Gravity Restore
+		PC.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
+		//Match PCs transform position
+		PC.transform.position = currentCheckPoint.transform.position;
+		//Show PC
+		player.SetActive(true);
+		pcRigid.GetComponent<Renderer> ().enabled = true;
+		//Spawn PC
+		Instantiate (respawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
+
+
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
 }
--- 2D Game/Assets/Scripts/kill.cs
+++ 2D Game/Assets/Scripts/kill.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class kill : MonoBehaviour {
 
+
+	public LevelManager levelManager;
+
+	// Use this for Initialization
+	void Start () {
+		levelManager = FindObjectOfType <LevelManager>();
+	}
 	void OnTriggerEnter2D(Collider2D other){
 		if(other.name == "Player"){
-			Destroy(other);
+			levelManager.RespawnPlayer();
 		}
 	}
 }
